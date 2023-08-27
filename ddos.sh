6c33473e6e6f4369234953f02e652e31514496d7
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-08-27 17:17:25 +0300

    Add files via upload

---
+++ ddos.sh
@@ -0,0 +1,17 @@
+#copyright : @an_danh
+# DDoS Tools Copright : an_danh
+#github : https://github.com/hacker2008274
+#telegram : 0978842516:@an_danh
+#we are no_team
+
+clear
+figlet ALQADHI_HK DDoS 
+echo -e "\e[3;84m[:::] ...::: DDoS attacakt : illegalchecker                :::... [:::]"
+echo -e "\e[2;52m[:::] ...::: github : https://github.com/FurkanGod        :::... [:::]"
+echo -e "\e[3;48m[:::] ...::: telegram : https://t.me/illegalchecker              :::... [:::]" 
+echo -e "\033[3;92m[:::]...::: Telegram :    [ @Furkanisyanedior ]                       :::... [:::]"
+echo -e "\e[1;34m[:::] kullanım (hedef sitenin url sini girin: https://url.com/)  : "
+read target
+echo -e "\033[3;93m[:::]         https://t.me/illegalchecker        [:::]"
+sleep 5
+go run 1.go -site $target
